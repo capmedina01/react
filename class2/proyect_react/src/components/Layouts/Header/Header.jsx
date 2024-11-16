0fb85906db8f278793d07e08d821bd51837a5f8f
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-11-16 12:02:32 -0500

    header logo

--- class2/proyect_react/src/components/Layouts/Header/Header.jsx
+++ class2/proyect_react/src/components/Layouts/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { LogoPrincipal } from "../../UI/LogoPrincipal/LogoPrincipal"
 import { Navbar } from "../../UI/Navbar/Navbar"
 
-Navbar
+
 export const Header = () => {
     return (
       <header>
+         <LogoPrincipal/>    
         <h1>Carlos Andres Perdomo Medina</h1>
         <Navbar/>        
       </header>
---
+++ class2/proyect_react/src/components/UI/LogoPrincipal/LogoPrincipal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LogoPrincipal = () =>{
+    return (
+        <img src='/src/assets/img/header/logo_portafolio.PNG' alt='logo'></img>
+    )
+}
